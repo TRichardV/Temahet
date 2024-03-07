8ee8f076e5c6d9a3df4c8f220cf8cadb7781cd12
kovacs_peter_gyorgy <kovacs_peter_gyorgy@A616-PC>
2024-03-07 10:53:02 +0100

    valami nem volt jo de most elvileg jo

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 document.getElementById("toggleButton").addEventListener("click", function() {
     var content = document.getElementById("content");
-    var button = document.getElementById("shuu");
+    var button = document.getElementById("toggleButton");
+    var text = document.getElementById("shuu");
     
     if (content.style.display === "none") {
         content.style.display = "block";
         button.style.display = "none"; 
+        text.style.display = "none"; 
+
     } else {
         content.style.display = "none";
     }
